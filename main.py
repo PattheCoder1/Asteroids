0b6c69aefb1507e1695ac773ae60542e6a9b1eb5
PatDevs <patirckhunter1995@gmail.com>
2025-04-04 14:08:01 -0400

    0.3: Added Player Character

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 
 from constants import *
+from player import *
 
 def main():
     pygame.init
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(F"Screen height: {SCREEN_HEIGHT}")
@@ -12,12 +16,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # While loop to run the game
-    while screen != 0:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        # Set the FPS to a max of 60
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
