7e3a539028e1213580da3cecc2213fe893782dcb
PatDevs <patirckhunter1995@gmail.com>
2025-04-04 13:16:21 -0400

    0.2: Added constants and game screen

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
 
+from constants import *
+
 def main():
+    pygame.init
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
+    
+    # Set screen settings
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # While loop to run the game
+    while screen != 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
